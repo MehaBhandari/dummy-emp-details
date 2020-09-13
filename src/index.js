8076ef37395231823f6d267f5b905bc8305e2bf9
Meha <ahem4progs@gmail.com>
2020-09-13 13:15:14 +0530

    created resuable empDetail component

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import EmpDetail from './empDetails';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function DummyApp() {
+  return (
+    <div>
+      <EmpDetail name='Meha' age='30' salary='100'></EmpDetail>
+    </div>
+  )
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<DummyApp></DummyApp>, document.getElementById('root'))
