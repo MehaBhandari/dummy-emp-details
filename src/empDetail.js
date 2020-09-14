89b198cd04aa3c1cb8551c83c5fe1dd152d5341c
Meha <ahem4progs@gmail.com>
2020-09-14 16:02:02 +0530

    added click functionality

--- src/empDetail.js
+++ src/empDetail.js
@@ -1,8 +1,13 @@
 import React from 'react';
 
 export default function EmpDetail(props) {
+    function divClicked() {
+        debugger;
+    }
+
     return(
-        <div onClick={() => alert(props.name)}>
+        <div onChange={divClicked}>
+            <input type='text' data-attr='emp.empId' value={props.empId} />
             <p>Name of employee is: {props.name}</p>
             <p>Age of employee is: {props.age}</p>
             <p>Salary of employee is: {props.salary}</p>
